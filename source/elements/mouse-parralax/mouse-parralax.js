9be673fc63e5960ae68d8d18fc999894527542a7
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2016-07-07 15:47:52 +0300

    add units

--- source/elements/mouse-parralax/mouse-parralax.js
+++ source/elements/mouse-parralax/mouse-parralax.js
@@ -1,17 +1,18 @@
 'use strict';
-var mouseMoveParralax = function (container, element, speed) {
+var mouseMoveParralax = function (container, element, speed, units) {
 	container.addEventListener('mousemove', function (event) {
 		var braking = speed || 5
+			, valueUnits = units || '%'
 			, x = event.clientX
 			, y = event.clientY
 			, containerWidth = this.offsetWidth
 			, containerHeight = this.offsetHeight
 			, xMousePositionFromCenter = Math.round(-((x/containerWidth)*100-50)/braking)
 			, yMousePositionFromCenter = Math.round(-((y/containerHeight)*100-50)/braking)
-			, translateValue = "translate(" + xMousePositionFromCenter + "%, " + yMousePositionFromCenter + "%" +")";
+			, translateValue = 'translate(' + xMousePositionFromCenter + valueUnits + ', ' + yMousePositionFromCenter + valueUnits +')';
 
 
 			element.style.transform = translateValue;
-			element.style["-webkit-transform"] = translateValue;
+			element.style['-webkit-transform'] = translateValue;
 	});
 };
