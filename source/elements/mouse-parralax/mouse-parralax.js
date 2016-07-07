e2921dc8008b510d0d000af6aac0da3765fe5fba
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2016-07-07 17:17:12 +0300

    add calculatePositionValue function

--- source/elements/mouse-parralax/mouse-parralax.js
+++ source/elements/mouse-parralax/mouse-parralax.js
@@ -11,17 +11,11 @@ var mouseMoveParralax = function (userParams) {
 
 	var params = this.extendParams(defaultParams, userParams);
 
+	var thisFunc = this;
 
 	params.container.addEventListener('mousemove', function (event) {
-		var	x = event.clientX
-			, y = event.clientY
-			, moveWayReltaiveMouse = params.isOposite ? -1 : 1
-			, containerWidth = this.offsetWidth
-			, containerHeight = this.offsetHeight
-			, xMousePositionFromCenter = Math.round(moveWayReltaiveMouse * ((x/containerWidth)*100-50)/params.braking)
-			, yMousePositionFromCenter = Math.round(moveWayReltaiveMouse * ((y/containerHeight)*100-50)/params.braking)
-			, translateValue = 'translate(' + xMousePositionFromCenter + params.units + ', ' + yMousePositionFromCenter + params.units +')';
 
+		var translateValue = thisFunc.calculatePositionValue(event, params);
 
 			params.element.style.transform = translateValue;
 			params.element.style['-webkit-transform'] = translateValue;
@@ -33,4 +27,18 @@ mouseMoveParralax.prototype.extendParams = function(defaultParams, userParams){
 		if(userParams.hasOwnProperty(key))
 			defaultParams[key] = userParams[key];
 	return defaultParams;
+};
+
+
+mouseMoveParralax.prototype.calculatePositionValue = function(event, params){
+	var	x = event.clientX
+		, y = event.clientY
+		, moveWayReltaiveMouse = params.isOposite ? -1 : 1
+		, containerWidth = params.container.offsetWidth
+		, containerHeight = params.container.offsetHeight
+		, xMousePositionFromCenter = Math.round(moveWayReltaiveMouse * ((x/containerWidth)*100-50)/params.braking)
+		, yMousePositionFromCenter = Math.round(moveWayReltaiveMouse * ((y/containerHeight)*100-50)/params.braking)
+		, translateValue = 'translate(' + xMousePositionFromCenter + params.units + ', ' + yMousePositionFromCenter + params.units +')'; 
+
+		return translateValue;
 };
